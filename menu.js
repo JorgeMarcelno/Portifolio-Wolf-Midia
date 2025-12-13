39d39c4027ca22cde3797bd9e0c927ca9749c4e0
JorgeMarcelno <154822964+JorgeMarcelno@users.noreply.github.com>
2025-12-13 11:12:05 +0200

    Primeiro commit do portfólio

---
+++ menu.js
@@ -0,0 +1,16 @@
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+let btnFechar = document.querySelector('.btn-fechar')
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu')
+})
+
+btnFechar.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
